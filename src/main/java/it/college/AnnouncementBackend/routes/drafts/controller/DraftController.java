7bc085aefd55eb16f7245e13177dfe0831c45b70
i22s0623 <i22s0623@students.it-college.ru>
2024-12-09 12:31:39 +0500

    Расписал контроллер для черновиков

--- src/main/java/it/college/AnnouncementBackend/routes/drafts/controller/DraftController.java
+++ src/main/java/it/college/AnnouncementBackend/routes/drafts/controller/DraftController.java
@@ -1,10 +1,13 @@
 package it.college.AnnouncementBackend.routes.drafts.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import it.college.AnnouncementBackend.routes.drafts.model.DraftPayload;
 import it.college.AnnouncementBackend.routes.drafts.servicies.DraftService;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Сервис черновиков объявлений")
 @RestController
@@ -13,4 +16,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class DraftController {
 
     private final DraftService service;
+
+    @GetMapping
+    @Operation(summary = "Получить все черновики пользователя")
+    public ResponseEntity findAllDraftByAuthorId(@RequestHeader("Authorization") String authorization){
+        return null;
+    }
+
+    @GetMapping("/{uuid}")
+    @Operation(summary = "Получить черновик по UUID")
+    public ResponseEntity findDraftByUUID(@RequestHeader("Authorization") String authorization,
+                                          @RequestParam("uuid") String uuid)
+    {
+        return null;
+    }
+
+    @PostMapping
+    @Operation(summary = "Сохранить черновик")
+    public ResponseEntity saveDraft(@RequestHeader("Authorization") String authorization,
+                                    @RequestParam(required = false) String uuid,
+                                    @RequestBody DraftPayload payload)
+    {
+        return null;
+    }
+
+    @DeleteMapping("/{uuid}")
+    @Operation(summary = "Удалить черновик")
+    public ResponseEntity deleteDraftByUUID(@RequestHeader("Authorization") String authorization,
+                                            @RequestParam("uuid") String uuid)
+    {
+        return null;
+    }
+
 }
---
+++ src/main/java/it/college/AnnouncementBackend/routes/drafts/model/DraftPayload.java
@@ -0,0 +1,8 @@
+package it.college.AnnouncementBackend.routes.drafts.model;
+
+import lombok.Data;
+
+@Data
+public class DraftPayload {
+    private String jsonContent;
+}
